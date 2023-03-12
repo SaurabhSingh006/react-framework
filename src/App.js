6e4ce2b6388ca6a42131a32b66b864cc9f1a5be9
saurabhsingh <saurabhsinghsix@gmail.com>
2023-03-12 18:46:49 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import SignIn from './pages/sign-in/SignIn';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Loader from './components/loader/Loader';
+
+// Defining the routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignIn />
+  },
+  {
+    path: '/home',
+    async lazy() {
+      const layout = await import('./pages/layout/Layout');
+      return { Component: layout }
+    }
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <Loader type="normal" backgroundColor='pink'  ></Loader>
-    </div>
+    <RouterProvider
+      router={router}
+      fallbackElement={ <Loader /> } >
+    </RouterProvider>
   );
 }
 
