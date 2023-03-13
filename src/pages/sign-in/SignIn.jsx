62e75a96a15a151193323f74c2868eaae1c9f807
saurabhsingh <saurabhsinghsix@gmail.com>
2023-03-13 11:59:29 +0530

    Update

--- src/pages/sign-in/SignIn.jsx
+++ src/pages/sign-in/SignIn.jsx
@@ -11,8 +11,17 @@ function SignIn() {
   })
 
   return (
-    <div className='bg-blue text-red'>
-      fghdfhjdff { userInfo.name }
+    <div className='flex flex-wrap h-screen text-white'>
+      <div className='w-screen md:w-1/2 sm:w-1/2 bg-primaryblue grid place-content-center'>
+        <div className='w-3/5 m-auto'>
+          <h1 className='text-4xl font-bold py-2'>Welcome to our community</h1>
+          <p className='text-slate-100'>Discover sustainable sportswear, athleisure and casualwear clothing for your active lifestyle. Jaspur connects you with the clothing you desire based on your values, while facilitating purchasing options that are most convenient for you.</p>
+        </div>
+      </div>
+
+      <div className='text-textcolor md:w-1/2'>
+        <h1>fghdfhjdff { userInfo.name }</h1>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primaryblue': '#0000ff',
+        'textcolor': '#1e293b'
+      }
+    },
   },
   plugins: [],
 }
