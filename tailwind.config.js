8b751ffc7467c66d2fa8aab41e03dd7a8c1a5986
saurabhsingh06 <saurabh.singh@vnnogile.in>
2023-03-22 20:06:00 +0530

    Update sign up

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,16 @@ module.exports = {
         '80': '8rem'
       },
       animation: {
-        'fade-out': 'fade .5s ease-in-out '
+        'fade-out': 'fade .5s ease-in-out ',
+        'ping': 'ping 1.2s cubic-bezier(1, 0, 0, 1) infinite'
       },
       keyframes: {
+        ping: {
+          '70%, 100%': {
+            transform: 'scale(1.3)',
+            opacity: 0
+          }
+        },
         fade: {
           'from': {
             transform: 'scale(0)',
